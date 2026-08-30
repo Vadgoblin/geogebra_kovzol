f79baf564f479d6eb380dbe68f6e93f235347eb8
vadgoblin@arch <vadgoblin@github.com>
2026-08-30 15:06:07 +0200

    clean up imports

--- common/src/main/java/org/geogebra/common/kernel/prover/ProverCNIMethod.java
+++ common/src/main/java/org/geogebra/common/kernel/prover/ProverCNIMethod.java
@@ -1,56 +1,15 @@
 package org.geogebra.common.kernel.prover;
 
-import java.util.ArrayList;
 import java.util.TreeSet;
 
-import org.geogebra.common.cas.GeoGebraCAS;
-import org.geogebra.common.kernel.Construction;
-import org.geogebra.common.kernel.Kernel;
-import org.geogebra.common.kernel.StringTemplate;
-import org.geogebra.common.kernel.algos.AlgoAnglePoints;
-import org.geogebra.common.kernel.algos.AlgoAngularBisectorPoints;
-import org.geogebra.common.kernel.algos.AlgoCircleThreePoints;
-import org.geogebra.common.kernel.algos.AlgoCircleTwoPoints;
-import org.geogebra.common.kernel.algos.AlgoDependentBoolean;
-import org.geogebra.common.kernel.algos.AlgoDependentPoint;
-import org.geogebra.common.kernel.algos.AlgoElement;
-import org.geogebra.common.kernel.algos.AlgoIntersectConics;
-import org.geogebra.common.kernel.algos.AlgoIntersectLineConic;
-import org.geogebra.common.kernel.algos.AlgoIntersectLines;
-import org.geogebra.common.kernel.algos.AlgoIntersectSingle;
-import org.geogebra.common.kernel.algos.AlgoLineBisector;
-import org.geogebra.common.kernel.algos.AlgoLineBisectorSegment;
-import org.geogebra.common.kernel.algos.AlgoLinePointLine;
-import org.geogebra.common.kernel.algos.AlgoMidpoint;
-import org.geogebra.common.kernel.algos.AlgoMidpointSegment;
-import org.geogebra.common.kernel.algos.AlgoMirror;
-import org.geogebra.common.kernel.algos.AlgoOrthoLinePointLine;
-import org.geogebra.common.kernel.algos.AlgoPointOnPath;
-import org.geogebra.common.kernel.algos.AlgoPolygonRegular;
-import org.geogebra.common.kernel.algos.AlgoRotatePoint;
-import org.geogebra.common.kernel.algos.AlgoTranslate;
-import org.geogebra.common.kernel.arithmetic.ExpressionNode;
-import org.geogebra.common.kernel.arithmetic.MySpecialDouble;
-import org.geogebra.common.kernel.geos.GeoAngle;
-import org.geogebra.common.kernel.geos.GeoConic;
 import org.geogebra.common.kernel.geos.GeoElement;
-import org.geogebra.common.kernel.geos.GeoLine;
-import org.geogebra.common.kernel.geos.GeoNumeric;
 import org.geogebra.common.kernel.geos.GeoPoint;
-import org.geogebra.common.kernel.geos.GeoSegment;
-import org.geogebra.common.kernel.geos.GeoVector;
 import org.geogebra.common.kernel.scripting.CmdShowProof;
-import org.geogebra.common.main.Localization;
-import org.geogebra.common.plugin.Operation;
-import org.geogebra.common.util.DoubleUtil;
 import org.geogebra.common.util.Prover;
 import org.geogebra.common.util.Prover.ProofResult;
-import org.geogebra.common.util.debug.Log;
-import org.geogebra.common.kernel.prover.ProverCNIMethodAsd;
 
-import static org.geogebra.common.cas.giac.CASgiac.ggbGiac;import static org.geogebra.common.kernel.prover.ProverCNIMethodAsd.PRIME;
+import static org.geogebra.common.kernel.prover.ProverCNIMethodAsd.PRIME;
 
-import com.himamis.retex.editor.share.util.Unicode;
 
 public class ProverCNIMethod {
 
@@ -81,7 +40,7 @@ public class ProverCNIMethod {
 			predefs += "[" + predefinition + "],";
 		}
 
-		context.predefinitions  = predefinitions;
+		context.predefinitions = predefinitions;
 		context.predefs = predefs;
 
 
@@ -110,8 +69,6 @@ public class ProverCNIMethod {
 							"Warning: Labels that already contain a prime symbol can cause display problems in later proof steps."));
 		}
 
-
-
 		return new ProverCNIMethodAsd().prove(context);
 	}
 
@@ -121,7 +78,7 @@ public class ProverCNIMethod {
 	 * @return the label as String
 	 */
 	static String getUniqueLabel(GeoElement ge) {
-		return ge.getLabelSimple().replace("_{","").replace("}", "");
+		return ge.getLabelSimple().replace("_{", "").replace("}", "");
 	}
 
 	private static boolean containsPrimedPointLabel(TreeSet<String> labels) {
