6b31e875f0527eda10a10b06c6cb810e2e4514a6
vadgoblin@arch <vadgoblin@github.com>
2026-08-30 21:07:45 +0200

    move CNIDefinition into separate file

--- common/src/main/java/org/geogebra/common/kernel/prover/CNIMethod/ProverCNIMethod.java
+++ common/src/main/java/org/geogebra/common/kernel/prover/CNIMethod/ProverCNIMethod.java
@@ -59,17 +59,6 @@ import org.geogebra.common.util.Prover.ProofResult;
 
 
 public class ProverCNIMethod implements ProverMethod {
-	public static class CNIDefinition {
-		// TODO: Consider adding more refinements here, add extra infos related to the Strings.
-		String declaration; // declaration in Giac format
-		String realRelation; // \n-separated Strings of lhs of real relations in Giac format
-		String zeroRelation; // lhs of zero relation in Giac format
-		String extraVariable; // an extra variable that is used in the zero relation (and the declaration)
-		boolean rMustBe0 = false; // if r is required to be 0
-		int warning = 0; // different interpretation than usual?
-		int specRestriction = 0; // number of disallowed fixed points
-	}
-	
 	private final ProverContext context;
 	List<String> declarations = new ArrayList<>();
 	List<String> realRelations = new ArrayList<>();
